938b4ade2099ce5e5e89454bd5917376e176d840
Thomas Frost <tom@fr0.st>
2020-10-08 13:53:57 -0700

    added cloudwatch error logging configuration

--- libs/handler-lib.js
+++ libs/handler-lib.js
@@ -1,23 +1,32 @@
-export default function handler(lambda) {
-    return async function (event, context) {
-      let body, statusCode;
+import * as debug from "./debug-lib";
 
-      try {
-        // Run the Lambda
-        body = await lambda(event, context);
-        statusCode = 200;
-      } catch (e) {
-        body = { error: e.message };
-        statusCode = 500;
-      }
-      // Return HTTP response
-      return {
-        statusCode,
-        body: JSON.stringify(body),
-        headers: {
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Credentials": true,
-        },
-      };
+export default function handler(lambda) {
+  return async function (event, context) {
+    let body, statusCode;
+
+    // Start debugger
+    debug.init(event, context);
+
+    try {
+      // Run the Lambda
+      body = await lambda(event, context);
+      statusCode = 200;
+    } catch (e) {
+      // Print debug messages
+      debug.flush(e);
+
+      body = { error: e.message };
+      statusCode = 500;
+    }
+
+    // Return HTTP response
+    return {
+      statusCode,
+      body: JSON.stringify(body),
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true,
+      },
     };
-  }
+  };
+}
